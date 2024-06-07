8713f3ecc1cc5e35642b0fd5c84e2a4b9130c7c8
githinjinjenga <githinjinjenga@outlook.com>
2024-06-07 15:40:16 +0300

    Initial Commit

---
+++ calc1/calc1App.swift
@@ -0,0 +1,17 @@
+//
+//  calc1App.swift
+//  calc1
+//
+//  Created by EMTECH MAC on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct calc1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
